7acb97f137f730e459193ecf3f2cbe58e3b1ab32
saheb9631 <92590505+saheb9631@users.noreply.github.com>
2026-02-04 18:08:55 +0530

    login page designed

---
+++ login_page/login_page/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  login_page
+//
+//  Created by Eazydiner on 04/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var text = ""
+    @State var pass = ""
+    var body: some View {
+        VStack {
+            Image("img1").resizable().frame(width: 200, height: 200)
+                .imageScale(.large)
+                .foregroundStyle(.tint).ignoresSafeArea().padding()
+            
+            Text("Login").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).bold()
+            Text("Why work hard if you can work smart").font(.subheadline)
+            
+            TextField("Enter Username", text: $text)
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 8).stroke(.black)
+                        
+                    
+                }
+            SecureField("password", text: $pass)
+                .padding()
+                .overlay{
+                    RoundedRectangle(cornerRadius: 8).stroke(.black)
+                }
+            
+            Button{
+                print("login")
+            }label: {
+                Text("login")
+                
+                
+                    .font(.title)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, minHeight: 50)
+                            .background(Color.blue)
+                            .cornerRadius(12)
+            }
+            Button{
+                print("forget")
+            }label: {
+                Text("Forget Password")
+            }
+            Spacer()
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ login_page/login_page/login_pageApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_pageApp.swift
+//  login_page
+//
+//  Created by Eazydiner on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct login_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
